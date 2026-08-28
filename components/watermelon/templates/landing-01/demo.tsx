783697ccb42cef03f17a2ba14c3ad1b019d9c603
Son1c-sound <makaradze98.s@gmail.com>
2026-08-28 02:20:19 -0400

    Reapply "Swap the landing hero for the hero-32 block"
    This reverts commit 9623503bf1a48a4678f83a9f950fda40222b2286.

--- components/watermelon/templates/landing-01/demo.tsx
+++ components/watermelon/templates/landing-01/demo.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Navbar from './landing/navbar';
 import Hero from './landing/hero';
 // Real pipeline numbers, parked until they are worth showing off.
 // import LiveData from './landing/live-data';
@@ -19,7 +18,6 @@ export default function Landing01Demo() {
       className="dark min-h-screen overflow-x-hidden bg-[#101010] font-sans"
       style={{ "--font-geist-mono": "var(--font-inter)" } as React.CSSProperties}
     >
-      <Navbar />
       <Hero />
       <Stats />
       <ManualVs />
